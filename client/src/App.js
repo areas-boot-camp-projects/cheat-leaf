92565a99e5bbbdb3e4824443e876d65b2a5345ee
wevexOP <111082615+wevexOP@users.noreply.github.com>
2023-02-28 10:02:12 -0800

    server works as intended and is able to render a webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    {(typeof backendData.users === 'undefined') ? (
+      <p>Loading...</p>
+    ): (
+      backendData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))
+    )
+    }
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
