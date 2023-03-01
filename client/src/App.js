7b0fffdeb8def6f4f71e07e4dfa38384ad5fabd3
Christian Areas <me@areas.me>
2023-02-28 20:02:26 -0800

    Added the Apollo Client dependencies and wrapped the app with the Apollo Provider.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from "react";
+// React.
+import React, { useState, useEffect } from "react"
+
+// Import the Apollo client.
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client"
+
+// Create the Apollo client.
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+})
 
 function App() {
 
+  // Fetch from the test route.
   const [backendData, setBackendData] = useState([{}])
-
   useEffect(() => {
     fetch("/api").then(
       response => response.json()
@@ -15,18 +29,20 @@ function App() {
   }, [])
 
   return (
-    <div>
-
-    {(typeof backendData.users === 'undefined') ? (
-      <p>Loading...</p>
-    ): (
-      backendData.users.map((user, i) => (
-        <p key={i}>{user}</p>
-      ))
-    )
-    }
-
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        {(typeof backendData.users === "undefined")
+          ? (
+            <p>Loading...</p>
+          )
+          : (
+            backendData.users.map((user, i) => (
+              <p key={i}>{user}</p>
+            ))
+          )
+        }
+      </div>
+    </ApolloProvider>
   )
 }
 
