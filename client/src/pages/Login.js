1a9740bdca382d1e0a6a12a17858ebd19b6c33b7
wevexOP <111082615+wevexOP@users.noreply.github.com>
2023-03-02 15:06:02 -0800

    Adding the start to a basic login page.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'react-bootstrap'
+
+
 export default function Login() {
-    return <h1>Login/Register Page</h1>
-}
+        return (
+            <Form style={{width:"80%", marginLeft:"10%", marginTop:"10%"}} >
+                <Form.Group >
+                    <Form.Label>Enter your email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter you email" />
+                </Form.Group>
+                <Form.Group >
+                    <Form.Label>Enter your password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter you password" />
+                </Form.Group>
+                <Button type="submit">Login</Button>
+            </Form>
+        )
+    }
