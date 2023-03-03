c10d6fd1aa1b5a1cd5ff7bd753fa0928dc2f6c69
Michael Virga <michael@studiomav.com>
2023-03-02 20:59:10 -0800

    navbar fix

--- client/src/nav/Navbar.js
+++ client/src/nav/Navbar.js
@@ -15,76 +15,78 @@ import logo from '../media/cheatleaflogo.png';
 
 
 export default class Navigation extends Component {
-  render() {
-    return (
-      <Router>
-        <Navbar className="nav-color" expand="lg">
-          <Container fluid >
-            <Navbar.Brand as={Link} to={"/Home"}>
-              <img
-                src={logo}
-                width="70"
-                height="30"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse className='justify-content-center' id="navbarScroll">>
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-              >
-                <Nav.Link as={Link} to={"/Home"}>Home</Nav.Link>
-                <Nav.Link as={Link} to={"/About"}>About</Nav.Link>
-                <NavDropdown title="Login" id="navbarScrollingDropdown">
-                  <NavDropdown.Item as={Link} to={"/Login"}>Login</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to={"/SignUp"}>
-                    Sign Up
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to={"/#"}>
-                    Sign Out
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to={"/MyProfile"}>
-                    My Profile
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Form className="d-flex search-bar">
-                <Form.Control
-                  type="search"
-                  placeholder="Search the forest of ideas... "
-                  className=" we-2 rounded-pill search-bar"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <div>
-            <Switch>
-            <Route path="/Home">
-                    <Home />
-            </Route>
-            <Route path="/About">
-                    <About />
-            </Route>
-            <Route path="/Login">
-                    <Login />
-            </Route>
-            <Route path="/MyProfile">
-                    <MyProfile />
-            </Route>
-            <Route path="/SignUp">
-                    <SignUp />
-            </Route>
-            </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+    render() {
+        return (
+            <Router>
+            <Navbar className="nav-color" expand="lg">
+              <Container fluid>
+                <Navbar.Brand as={Link} to={"/Home"}>
+                  <img
+                    src={logo}
+                    width="70"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="React Bootstrap logo"
+                  />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse className='justify-content-center' id="navbarScroll">
+                  <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                  >
+                    <Nav.Link as={Link} to={"/Home"}>Home</Nav.Link>
+                    <Nav.Link as={Link} to={"/About"}>About</Nav.Link>
+                  </Nav>
+                  <Form className="d-flex ml-5 mr-5 w-50"
+                        style={{ alignItems: 'center'}}>
+
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2 rounded-pill"
+                      aria-label="Search"
+                    />
+                    <Button>Search</Button>
+                  </Form>
+                  <NavDropdown title="Login" id="navbarScrollingDropdown">
+                      <NavDropdown.Item as={Link} to={"/Login"}>Login</NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to={"/SignUp"}>
+                        Sign Up
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item as={Link} to={"/#"}>
+                        Sign Out
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item as={Link} to={"/MyProfile"}>
+                        My Profile
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+            <div>
+                <Switch>
+                <Route path="/Home">
+                        <Home />
+                </Route>
+                <Route path="/About">
+                        <About />
+                </Route>
+                <Route path="/Login">
+                        <Login />
+                </Route>
+                <Route path="/MyProfile">
+                        <MyProfile />
+                </Route>
+                <Route path="/SignUp">
+                        <SignUp />
+                </Route>
+                </Switch>
+            </div>
+            </Router>
+          );
+        }
+    }
