6018d49dd977f3895c7d9471e06bd3418ff70443
james <arvinjamesinanoria@gmail.com>
2023-03-05 23:37:37 -0800

    profile page

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,3 +1,82 @@
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
+import backgroundImage from '../media/forestimg.jpg';
+import pfp from '../media/daboy.jpg'
+
 export default function MyProfile() {
-    return <h1>My Profile</h1>
+  return (
+    <div className='d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: "url(" + backgroundImage + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol lg="9" xl="7">
+            <MDBCard style={{backgroundColor: '#F4F1E6'}}>
+              <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#493E20', height: '200px' }}>
+                <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
+                  <MDBCardImage src={pfp}
+                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                  <MDBBtn outline color="dark" style={{height: '36px', overflow: 'visible'}}>
+                    Edit profile
+                  </MDBBtn>
+                </div>
+                <div className="ms-3" style={{ marginTop: '130px' }}>
+                  <MDBTypography className='profile-name' tag='h5' style={{marginBottom:'3px'}}>Da Boy</MDBTypography>
+                  <MDBCardText>San Francisco</MDBCardText>
+                </div>
+              </div>
+              <div className="p-4 text-white" style={{ backgroundColor: '#B5A478' }}>
+                <div className="d-flex justify-content-end text-center py-1">
+                  <div>
+                    <MDBCardText className="mb-1 h5">284</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Leaves</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">19</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Branches</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">1202</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                </div>
+              </div>
+              <MDBCardBody className="text-black p-4">
+                <div className="mb-5">
+                  <p className="lead fw-normal mb-1">About</p>
+                  <div className="p-4" style={{ backgroundColor: '#F4F1E6' }}>
+                    <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
+                    <MDBCardText className="font-italic mb-1">Works at Apple</MDBCardText>
+                    <MDBCardText className="font-italic mb-0">UC Berkeley</MDBCardText>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mb-4">
+                  <MDBCardText className="lead fw-normal mb-0">Recent Leaves</MDBCardText>
+                  <MDBCardText className="mb-0"><a href="#!" className="text-muted">Show all</a></MDBCardText>
+                </div>
+                <MDBRow>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src=" "
+                      alt=" " className="w-100 rounded-3" />
+                  </MDBCol>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src=" "
+                      alt=" " className="w-100 rounded-3" />
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow className="g-2">
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src=" "
+                      alt=" " className="w-100 rounded-3" />
+                  </MDBCol>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src=" "
+                      alt=" " className="w-100 rounded-3" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
 }
