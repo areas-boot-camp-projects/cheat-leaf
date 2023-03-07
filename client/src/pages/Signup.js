b524af03785d1517b4c0f4e8954ebe347039e6db
Christian Areas <me@areas.me>
2023-03-06 23:53:51 -0800

    Added logic to sign up a user and save their token to local storage.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,103 +1,112 @@
-import React from 'react'
-import axios from '../api/axios';
-import { setAuthToken } from "../helpers/setAuthToken";
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBCheckbox
-}
-from 'mdb-react-ui-kit';
-import backgroundImage from '../media/forestimg.jpg';
+// React.
+import React, { useState } from "react"
 
-function Signup() {
+// UI.
+import { MDBBtn, MDBContainer, MDBCard, MDBCardBody, MDBInput, MDBCheckbox } from "mdb-react-ui-kit"
+import backgroundImage from "../media/forestimg.jpg"
 
-    /*const [validated, setValidated] = useState(false);
+// API and authentication.
+import { useMutation } from "@apollo/client"
+import { SIGN_UP_USER } from "../gql/mutations"
+import { saveTokenToLocalStorage } from "../helpers/auth"
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+// Component.
+function SignUp() {
+  // Set the form data initial state.
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
+
+  // Update the form data state.
+  function updateFormData(e) {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  // Set up the mutation.
+  const [signUpUser] = useMutation(SIGN_UP_USER)
+
+  // Sign up the user.
+  async function submitFormData(e) {
+    e.preventDefault()
+    try {
+      const { data } = await signUpUser({
+        variables: { ...formData },
+      })
+      saveTokenToLocalStorage(data.signUpUser.token)
+    } catch (err) {
+      console.log(err)
     }
-
-    setValidated(true);
-  }; */
+    // Clear the form data.
+    setFormData({
+      username: "",
+      email: "",
+      password: "",
+    })
+  }
 
   return (
-    <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image min-vh-100' style={{backgroundImage: "url(" + backgroundImage + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-      <div className='mask gradient-custom-3'></div>
-      <MDBCard className='m-5' style={{borderRadius: '1rem', maxWidth: '500px'}}>
-        <MDBCardBody className='p-5'>
+    <MDBContainer fluid className="d-flex align-items-center justify-content-center bg-image min-vh-100" style={{backgroundImage: "url(" + backgroundImage + ")", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+      <div className="mask gradient-custom-3"></div>
+      <MDBCard className="m-5" style={{borderRadius: "1rem", maxWidth: "500px"}}>
+        <MDBCardBody className="p-5">
           <h2 className="fw-bold text-center mb-5">Sign Up</h2>
-          <MDBInput wrapperClass='mb-2' label='Your Name' size='lg' id='form1' type='text'/>
-          <MDBInput wrapperClass='mb-2' label='Your Email' size='lg' id='form2' type='email'/>
-          <MDBInput wrapperClass='mb-2' label='Password' size='lg' id='form3' type='password'/>
-          <MDBInput wrapperClass='mb-4' label='Repeat your password' size='lg' id='form4' type='password'/>
-          <div className='d-flex flex-row justify-content-center mb-4'>
-            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
+          
+          <MDBInput
+            wrapperClass="mb-2"
+            label="Username"
+            size="lg"
+            id="form1"
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={updateFormData}
+          />
+          
+          <MDBInput
+            wrapperClass="mb-2"
+            label="Email"
+            size="lg"
+            id="form2"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={updateFormData}
+          />
+          
+          <MDBInput
+            wrapperClass="mb-2"
+            label="Password"
+            size="lg"
+            id="form3"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={updateFormData}
+          />
+                    
+          <div className="d-flex flex-row justify-content-center mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              id="flexCheckDefault"
+              label="I agree to all statements in terms of service."
+            />
           </div>
-          <MDBBtn className='mb-5 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+          
+          <MDBBtn
+            className="mb-5 w-100 gradient-custom-4"
+            size="lg"
+            onClick={submitFormData}
+          >
+            Sign Up
+          </MDBBtn>
+
         </MDBCardBody>
       </MDBCard>
     </MDBContainer>
-  );
+  )
 }
 
-
-
-
-const onSubmit = (email, password) => {
-  const loginPayload = {
-      email: ' ',
-      password: ' '
-  }
-
-  axios.post("", loginPayload)
-      .then(response => {
-      const token = response.data.token;
-  
-      localStorage.setItem("token", token);
-
-      setAuthToken(token);
-
-      window.location.href = '/'
-  })
-  .catch(err => console.log(err));
-};
-
-export default Signup;
-
-
-
-/* import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'react-bootstrap'
-
-export default function SignUp() {
-        return (
-            <Container>
-                <Row>
-                    <Col></Col>
-                    <Col className='pt-3'>
-                        <Form>
-                            <Form.Group >
-                                <Form.Label>Enter your email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter you email" />
-                            </Form.Group>
-                            <Form.Group >
-                                <Form.Label>Create a password</Form.Label>
-                                <Form.Control type="password" placeholder="Enter you password" />
-                            </Form.Group>
-                            <Button type="submit">Sign Up!</Button>
-                        </Form>
-                    </Col>
-                    <Col></Col>
-                </Row>
-            </Container>
-        )
-    }
-*/
-
-
-
+export default SignUp
