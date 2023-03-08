07dff17c2c68fb60ce92c722b6989d8d9e9b9fbb
kait <kingkittykait@gmail.com>
2023-03-07 20:46:51 -0800

    adjusted server file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // Dependencies.
 const mongoose = require("mongoose")
 const db = "cheatLeaf"
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${db}`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://127.0.0.1/${db}`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		})
