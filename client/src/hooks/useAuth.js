0e7f525353d4512a669b5626530f90cdd2a042a8
kait <kingkittykait@gmail.com>
2023-03-07 19:30:26 -0800

    added startr script

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext, useDebugValue } from "react";
-import AuthContext from "../context/AuthProvider";
+import AuthContext from "../context/authContext";
 
 const useAuth = () => {
     const { auth } = useContext(AuthContext);
